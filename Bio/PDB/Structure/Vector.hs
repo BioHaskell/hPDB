30001001af8464802b7d72ca04b2ddded58813a1
Michal J. Gajda <mjgajda@mail.google.com>
2013-09-17 23:02:58 +0200

    Vec3D -> Vector3.

--- Bio/PDB/Structure/Vector.hs
+++ Bio/PDB/Structure/Vector.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoMonomorphismRestriction, BangPatterns #-}
-module Bio.PDB.Structure.Vector(Vec3D(..),
-                                unpackVec3D,
+module Bio.PDB.Structure.Vector(Vector3(..),
+                                unpackVector3,
                                 vnormalise, vdot, (*|), (|*),
                                 vzip, vmap,
                                 vnorm, vproj, vperpend, vperpends, vdihedral) where
@@ -10,54 +10,51 @@ import Data.Vector.V3
 import Data.List(foldl')
 import Test.QuickCheck
 
--- ^ This module wraps 3D vector operations, and adds missing ones. Also hides a "Vector" class
-
--- | Defines type alias for position and translation vectors in PDB structures.
-type Vec3D = Vector3
+-- ^ This module wraps 3D vector operations, and adds missing ones.
 
 -- | Unpacks an abstract 3D vector into a triple of Doubles.
-unpackVec3D :: Vec3D -> (Double, Double, Double)
-unpackVec3D (Vector3 x y z) = (x, y, z)
+unpackVector3 :: Vector3 -> (Double, Double, Double)
+unpackVector3 (Vector3 x y z) = (x, y, z)
 
 -- | Maps an operation that modifies a Double onto a 3D vector.
 {-# INLINE vmap #-}
-vmap :: (Double -> Double) -> Vec3D -> Vec3D
+vmap :: (Double -> Double) -> Vector3 -> Vector3
 vmap = C.vmap
 
 -- | Maps an operation on a pair of Doubles onto a pair of 3D vectors
 --   coordinatewise.
-vzip :: (Double -> Double -> Double) -> Vec3D -> Vec3D -> Vec3D
+vzip :: (Double -> Double -> Double) -> Vector3 -> Vector3 -> Vector3
 vzip = C.vzip
 
 -- | Normalises to a unit vector in the same direction as input.
 {-# INLINE vnormalise #-}
-vnormalise :: Vec3D -> Vec3D
+vnormalise :: Vector3 -> Vector3
 vnormalise = C.vnormalise 
 
 -- | Computes a dot product of two 3D vectors.
 {-# INLINE vdot #-}
-vdot :: Vec3D -> Vec3D -> Double
+vdot :: Vector3 -> Vector3 -> Double
 vdot = C.vdot
 
 {-# INLINE vnorm #-}
 -- | 2-norm of a vector (also called a magnitude or length.)
-vnorm :: Vec3D -> Double
+vnorm :: Vector3 -> Double
 vnorm = C.vmag
 
 {-# INLINE vdihedral #-}
 -- | Compute dihedral between three bond vectors using spherical angle formula.
-vdihedral :: Vec3D -> Vec3D -> Vec3D -> Double
+vdihedral :: Vector3 -> Vector3 -> Vector3 -> Double
 vdihedral !a !b !c = atan2 (vnorm b * (a `vdot`  (b `vcross` c)))
                            ((a `vcross` b) `vdot` (b `vcross` c))
 
 -- | Scalar product. (`*` indicates side on which one can put a scalar.)
 {-# INLINE (*|) #-}
-(*|) :: Double -> Vec3D  -> Vec3D
+(*|) :: Double -> Vector3  -> Vector3
 (*|) = (C.*|)
 
 -- | Scalar product. (`*` indicates side on which one can put a scalar.)
 {-# INLINE (|*) #-}
-(|*) :: Vec3D  -> Double -> Vec3D
+(|*) :: Vector3  -> Double -> Vector3
 (|*) = (C.|*)
 
 {-# INLINE vproj #-}
