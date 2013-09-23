4fa321551624b680d63ac3d648be1f252a5f8e21
Michal J. Gajda <mjgajda@gmail.com>
2013-09-23 13:37:08 +0200

    Reformatting in StrandSense.

--- Bio/PDB/EventParser/StrandSense.hs
+++ Bio/PDB/EventParser/StrandSense.hs
@@ -1,8 +1,8 @@
-{-| Module with enumeration of beta-strand senses. -}
+-- | Module with enumeration of beta-strand senses.
 module Bio.PDB.EventParser.StrandSense(StrandSenseT(Parallel, Antiparallel))
 where
 
-{-| Enumeration of beta-strand sense. -}
+-- | Enumeration of beta-strand sense.
 data StrandSenseT = Parallel |
                     Antiparallel 
   deriving (Eq, Ord, Show, Read)
