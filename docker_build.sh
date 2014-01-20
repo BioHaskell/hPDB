296404a66e002cde3ba79bc43b9b31da34dc91ae
Michal J. Gajda <mjgajda@gmail.com>
2014-01-20 09:16:33 +0100

    Attempt to provide separate dist/ volume for Docker.

--- docker_build.sh
+++ docker_build.sh
@@ -2,4 +2,4 @@
 
 #docker run -v `pwd`:`pwd` -w `pwd` afriel/ghc-head-dynamic bash -c "sudo apt-get install -y zlib1g-dev libstdc++-dev; cabal update && cabal install "
 
-docker run -v `pwd`:`pwd` -w `pwd` afriel/ghc-head-dynamic bash -c "sudo apt-get install -y zlib1g-dev libstdc++-4.8-dev libstdc++6-4.7-dev g++; cabal update && cabal install"
+docker run -v `pwd`:`pwd` -v `pwd`/dist -w `pwd` afriel/ghc-head-dynamic bash -c "sudo apt-get install -y zlib1g-dev libstdc++-4.8-dev libstdc++6-4.7-dev g++; cabal update && cabal install"
