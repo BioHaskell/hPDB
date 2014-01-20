c1665c3fa8f993c462235b72c492f8e4f96ba6ce
Michal J. Gajda <mjgajda@gmail.com>
2014-01-20 09:03:57 +0100

    First attempt at building with Docker on GHC HEAD.

---
+++ docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#docker run -v `pwd`:`pwd` -w `pwd` afriel/ghc-head-dynamic bash -c "sudo apt-get install -y zlib1g-dev libstdc++-dev; cabal update && cabal install "
+
+docker run -v `pwd`:`pwd` -w `pwd` afriel/ghc-head-dynamic bash -c "sudo apt-get install -y zlib1g-dev libstdc++-4.8-dev libstdc++6-4.7-dev g++; cabal update && cabal install"
