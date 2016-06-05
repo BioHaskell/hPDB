dc258a9df16abfd19f7410b726229f533c447830
Michal J. Gajda <mjgajda@gmail.com>
2016-06-05 16:50:44 +0800

    Remove old parallel benchmark.

--- parallel/test_parallel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-time (ghc -O3 -threaded -rtsopts test_parallel.hs && time ./test_parallel 3JYV.pdb && time ./test_parallel 3JYV.pdb +RTS -N)
-#ghc -O3 -rtsopts test_parallel.hs && time ./test_parallel 1HTQ.pdb
--- test_parallel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-time (ghc -O3 -threaded -rtsopts tests/test_parallel.hs &&
-      time tests/test_parallel 3JYV.pdb                 &&
-      time tests/test_parallel 3JYV.pdb +RTS -N           )
-#ghc -O3 -rtsopts test_parallel.hs && time ./test_parallel 1HTQ.pdb
