e7e490170a7ee7f3ee79d00a8eed51e3cf9a4ad1
kanishka <nish2575@gmail.com>
2018-12-21 19:20:23 -0500

    First pointless test to get warmed up - make an atom

---
+++ test/Bio/PDB/StructureTest.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Bio.PDB.StructureTest where
+
+import Linear
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Bio.PDB.Structure
+
+atom1 :: Atom
+atom1 = Atom {
+    atName = "nm1"
+  , atSerial = 1
+  , coord = V3 1 2 3
+  , bFactor = 2.0
+  , occupancy = 3.0
+  , element = "elem"
+  , segid = "sid1"
+  , charge = "chrg"
+  , hetatm = True
+  }
+
+unit_atom1 :: IO ()
+unit_atom1 = atom1 @?= atom1
